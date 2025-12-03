15367b5f1d997704f06ff78f595c103fe752ba8f
gdornevil <kingasphalt08@gmail.com>
2025-12-03 18:27:24 -0500

    Update CardType05Favor.java

--- src/main/java/gametest1/gametest1/CardType05Favor.java
+++ src/main/java/gametest1/gametest1/CardType05Favor.java
@@ -6,12 +6,20 @@ package gametest1.gametest1;
 
 /**
  *
- * @author chris
+ * @author Gianni
  */
 public class CardType05Favor extends Card{
     public CardType05Favor(){
         this.cardName="Favor";
         this.cardId=5;
         this.cardDescription="One playe gives you a card of their choice.";
+    }
+    public void resolve(Game game, Player user) {
+        Player target = game.chooseOtherPlayer(user);
+
+        Card given = target.currentHand.hand.pop(); // random or first
+        user.AddCardToHand(given);
+
+        System.out.println(target.playerName + " gave a card to " + user.playerName);
     }
 }
