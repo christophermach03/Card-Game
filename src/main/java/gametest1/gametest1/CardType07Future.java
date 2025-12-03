e2c91d6721d3c51abf6d5fbde163e7dd5cdbb1b4
gdornevil <kingasphalt08@gmail.com>
2025-12-03 18:26:04 -0500

    Update CardType07Future.java

--- src/main/java/gametest1/gametest1/CardType07Future.java
+++ src/main/java/gametest1/gametest1/CardType07Future.java
@@ -4,14 +4,20 @@
  */
 package gametest1.gametest1;
 
+import java.util.List;
+
 /**
  *
- * @author chris
+ * @author Gianni
  */
 public class CardType07Future extends Card{
     public CardType07Future(){
         this.cardName="See the Future";
         this.cardId=0;
         this.cardDescription="Look at the top three cards of the deck.";
+    }
+    public void resolve(Game game, Player user) {
+        List<Card> topCards = game.currentDeck.peekTop(3);
+        System.out.println("Top 3 cards: " + topCards);
     }
 }
